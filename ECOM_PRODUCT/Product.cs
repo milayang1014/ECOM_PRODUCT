dc63394385556bb23c47808b3ca0156f994e2ba0
milayang1014 <milayang1991@gmail.com>
2024-06-12 10:18:26 -0400

    Completed Product class implementation

---
+++ ECOM_PRODUCT/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECOM_PRODUCT
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(int productId, string productName, decimal price, int stock)
+        {
+            ProductID = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            Stock += amount;
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            Stock -= amount;
+        }
+    }
+}
